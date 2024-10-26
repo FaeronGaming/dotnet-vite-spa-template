9829aa1f4c9c3375a7e6c9a03a2dd3a81f0703bc
FaeronGaming <animeniac@live.com>
2024-10-26 12:12:30 -0600

    Serve basic api response for testing

--- src/dotnet-web-react-vite-template.Web/Controllers/HomeController.cs
+++ src/dotnet-web-react-vite-template.Web/Controllers/HomeController.cs
@@ -1,31 +1,12 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using dotnet_web_react_vite_template.Web.Models;
 
 namespace dotnet_web_react_vite_template.Web.Controllers;
 
-public class HomeController : Controller
+[Route("api/[controller]")]
+[ApiController]
+public class HomeController : ControllerBase
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
-
-    public IActionResult IndexOld()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+    // GET: api/home/name
+    [HttpGet("name")]
+    public ActionResult<string> GetName() => "Test";
 }
